7f6cf7e791059c8e621193a08114ba3fc03296f2
Rami Sayar <rami.sayar@gmail.com>
2013-03-29 00:12:17 -0400

    Added mongodb host, port to settings.py

--- settings.py
+++ settings.py
@@ -4,3 +4,5 @@ import os
 
 TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
+MONGODB_HOST = os.environ.get('MONGODB_HOST', 'localhost')
+MONGODB_PORT = os.environ.get('MONGODB_PORT', 27017)
