458c4885b64be2149cfbdaef8c0f35cd97ed42e0
Rami Sayar <rami.sayar@gmail.com>
2013-03-28 23:04:06 -0400

    Added license, requirements and settings file.

---
+++ settings.py
@@ -0,0 +1,6 @@
+# author: Rami Sayar, Dominik Laskowski
+
+import os
+
+TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
+TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
