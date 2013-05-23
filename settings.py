7e21896227b2f47224b9e7a0a031b793e8be55c5
Rami Sayar <rami.sayar@gmail.com>
2013-05-23 16:18:41 -0400

    Renamed mongodb. Reverted back to insert.

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID', None)
 TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
 
 #NOTE: The mongodb will be deleted right before this repo goes publis so I don't care if you can see the password.
-MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://ted:ted@sawyer.mongohq.com/TEDxHEC')
+MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://ted:ted@sawyer.mongohq.com/tedxhec')
 MONGODB_PORT = os.environ.get('MONGODB_PORT', 10015)
 
 #NOTE: This is also going down... sorry internet.
