a07cf19774c31fd1ead41b6a8476c50f1821ffd1
Rami Sayar <rami.sayar@gmail.com>
2013-05-23 15:10:18 -0400

    Added settings for redis.

--- settings.py
+++ settings.py
@@ -9,5 +9,6 @@ TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN', None)
 MONGODB_HOST = os.environ.get('MONGODB_HOST', 'mongodb://ted:ted@sawyer.mongohq.com/TEDxHEC')
 MONGODB_PORT = os.environ.get('MONGODB_PORT', 10015)
 
-REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+#NOTE: This is also going down... sorry internet.
+REDIS_HOST = os.environ.get('REDIS_HOST', 'ec2-107-22-63-170.compute-1.amazonaws.com')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
